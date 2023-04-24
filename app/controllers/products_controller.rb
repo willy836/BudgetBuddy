439d5ecd2100bc6b3ea604929f3ae3115ca870fd
willy836 <wilsonkimiri836@gmail.com>
2023-04-24 14:52:09 +0300

    Generate controllers and add routes

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get products_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get products_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get products_destroy_url
+    assert_response :success
+  end
+end
