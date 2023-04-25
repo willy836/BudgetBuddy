1fc0b2e966f1d887960664b86694b7706c8436d9
willy836 <wilsonkimiri836@gmail.com>
2023-04-25 11:19:11 +0300

    Style categories/show view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :category_products
-  has_many :categories, through: :category_products
+  has_many :category_products, dependent: :destroy
+  has_many :categories, through: :category_products, dependent: :destroy
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
