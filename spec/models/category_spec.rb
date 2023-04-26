73a42fbe51e7c84f3fcde4cc48dd3fa43c4eef2c
willy836 <wilsonkimiri836@gmail.com>
2023-04-26 07:17:00 +0300

    Add tests for custom methods in category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  subject do
-    Category.new(name: 'Nando Restaurant')
-  end
+  subject { Category.new(name: 'Test Category') }
 
   before { subject.save }
 
-  it 'should have a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  describe 'validations' do
+    it 'should have a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#total_amount' do
+    it 'returns total_amount of a category' do
+      expect(subject.total_amount).to eq(subject.products.sum(:amount))
+    end
+  end
+
+  describe '#recent_products' do
+    it 'returns recent products ordered in descending order' do
+      expect(subject.recent_products).to eq(subject.products.order(created_at: :desc))
+    end
   end
 end
